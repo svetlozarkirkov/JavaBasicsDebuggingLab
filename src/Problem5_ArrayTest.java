82d14bf5c1d8a651dc16fb5b99c920b7616bde7d
Svetlozar Kirkov <svetlozark@gmail.com>
2015-01-26 04:44:21 +0200

    modified:   bin/Problem5_ArrayTest.class 	modified:   src/Problem5_ArrayTest.java

--- src/Problem5_ArrayTest.java
+++ src/Problem5_ArrayTest.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Problem5_ArrayTest {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
@@ -15,18 +14,24 @@ public class Problem5_ArrayTest {
 		
 		String command = scn.next();
 
-		while (!command.equals("over")) {
+		while (!command.equals("stop")) {   // stop, not "over" :)
 			String line = scn.nextLine().trim();
 			int[] params = new int[2];
 
-			if(command.equals("add") || command.equals("substract") || command.equals("multiply")) {
+			if(command.equals("add") || command.equals("subtract") || command.equals("multiply")) {  // was "substract" :D
 				String[] stringParams = line.split(" ");
 				params[0] = Integer.parseInt(stringParams[0]);
 				params[1] = Integer.parseInt(stringParams[1]);
 
 				performAction(array, command, params);
 			}
-			performAction(array, command, params);
+			else if (command.equals("rshift")){
+				arrayShiftRight(array);
+			}
+			else if (command.equals("lshift")){
+				arrayShiftLeft(array);
+			}
+			//performAction(array, command, params); // no need for this
 
 
 			printArray(array);
@@ -37,39 +42,43 @@ public class Problem5_ArrayTest {
 	}
 	
 	static void performAction(long[] arr, String action, int[] params){
-		long[] array = arr.clone();
-		int pos = params[0];
+		//long[] array = arr.clone(); // if we clone the array and do not return it , there's no point
+		int pos = params[0]-1; // the position "i" starts from 1, so we subtract 1
 		int value = params[1];
 
 		switch (action) {
 		case "multiply":
-			array[pos] *= value;
+			arr[pos] *= value;		
 			break;
 		case "add":
-			array[pos] += value;
+			arr[pos] += value; 
 			break;
 		case "subtract":
-			array[pos] -= value;
+			arr[pos] -= value;
 			break;
-		case "lshift":
-			arrayShiftLeft(array);
-			break;
-		case "rshift":
-			arrayShiftRight(array);
-			break;
+		//case "lshift":				// no need for these as there are methods below for shifting
+		//	arrayShiftLeft(arr);
+		//	break;
+		//case "rshift":
+		//	arrayShiftRight(arr);
+		//	break;
 		}
 	}
 
 	private static void arrayShiftRight(long[] array) {
+		long tempNum = array[array.length-1]; // keeping the last number in the array
 		for (int i = array.length-1; i >= 1 ; i--) {
 			array[i] = array[i - 1];
 		}
+		array[0] = tempNum; // the last number goes to first places
 	}
 
 	private static void arrayShiftLeft(long[] array) {
+		long tempNum = array[0]; // keeping the first number in the array
 		for (int i = 0; i < array.length - 1; i++) {
 			array[i] = array[i+1];
 		}
+		array[array.length-1]=tempNum;  // the first number goes to last place
 	}
 
 	private static void printArray(long[] array) {
